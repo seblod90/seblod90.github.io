103444fafed505f733cc8762f92306109e1900ae
Sebastian Loder <36483752+seblod90@users.noreply.github.com>
2020-08-07 19:04:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ for (s = 1; s < 5; s++) {
   	
 
 $.getJSON('js/yarns.json', function(data){
-console.log(data);
 var $yarn_selection = $("#yarn" + s);
   $yarn_selection.append('<option selected value="base">Bitte auswählen</option>')
   $(data).each(function(i, entry){
@@ -24,13 +23,14 @@ var $yarn_selection = $("#yarn" + s);
 
 
 $.getJSON('js/colors.json', function(data){
-console.log(data);
 var $color_selection = $("#color" + s);
   $color_selection.append('<option selected value="base">Bitte auswählen</option>')
   $(data).each(function(i, entry){
 	  $color_selection.append("<option>" + entry.name + "</option>");
   });
 });
+
+console.log("#amount" + s)
 
 var $amount_selection = $("#amount" + s);
   for (i = 0; i < 11; i++) {
