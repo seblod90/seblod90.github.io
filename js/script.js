aba7408d240383c7ef973d4008a618d605e3c67b
Sebastian Loder <36483752+seblod90@users.noreply.github.com>
2020-08-07 18:45:21 +0200

    automatic fill for 5 selection rows

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
 
-/*
-$.getJSON('js/data.json', function(data){
-console.log(data);
-$("#demo").html("JUUUUHU");
-var $secondChoice = $("#second-choice");
-  $(data).each(function(i, hero){
-	  $secondChoice.append("<option>" + hero.Name + "</option>");
-  });
-})
-*/
+for (s = 1; s <= 5; s++) {
 
-$.getJSON('js/yarns.json', function(data){
-console.log(data);
-var $yarn_selection = $("#yarn1");
-  $yarn_selection.append('<option selected value="base">Bitte auswählen</option>')
-  $(data).each(function(i, entry){
-	  $yarn_selection.append("<option>" + entry.name + "</option>");
-  });
-})
+	$.getJSON('js/yarns.json', function(data){
+	console.log(data);
+	var $yarn_selection = $("#yarn" + s);
+	  $yarn_selection.append('<option selected value="base">Bitte auswählen</option>')
+	  $(data).each(function(i, entry){
+		  $yarn_selection.append("<option>" + entry.name + "</option>");
+	  });
+	})
+	
+	
+	$.getJSON('js/colors.json', function(data){
+	console.log(data);
+	var $color_selection = $("#color" + s);
+	  $color_selection.append('<option selected value="base">Bitte auswählen</option>')
+	  $(data).each(function(i, entry){
+		  $color_selection.append("<option>" + entry.name + "</option>");
+	  });
+	})
+	
+	var $amount_selection = $("#amount" + s);
+	  for (i = 0; i < 11; i++) {
+		$amount_selection.append("<option>" + i + "</option>");	
+	}
 
-
-$.getJSON('js/colors.json', function(data){
-console.log(data);
-var $color_selection = $("#color1");
-  $color_selection.append('<option selected value="base">Bitte auswählen</option>')
-  $(data).each(function(i, entry){
-	  $color_selection.append("<option>" + entry.name + "</option>");
-  });
-})
-
-var $amount_selection = $("#amount1");
-  for (i = 0; i < 11; i++) {
-  	$amount_selection.append("<option>" + i + "</option>");	
 }
