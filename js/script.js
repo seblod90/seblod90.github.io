05f7591094a3e3cd7761e4f6d7a9a1149f1c09dd
Sebastian Loder <36483752+seblod90@users.noreply.github.com>
2020-08-07 19:01:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
 
-for (s = 0; s < 11; s++) {
-	console.log(s);
-var s = 1;
-	$.getJSON('js/yarns.json', function(data){
-	//console.log(data);
-	var $yarn_selection = $("#yarn" + s);
-	  $yarn_selection.append('<option selected value="base">Bitte Garn wählen</option>')
-	  $(data).each(function(i, entry){
-		  $yarn_selection.append("<option>" + entry.name + "</option>");
-	  });
-	});
-	
-	
-	$.getJSON('js/colors.json', function(data){
-	var $color_selection = $("#color" + s);
-	  $color_selection.append('<option selected value="base">Bitte Farbe wählen</option>')
-	  $(data).each(function(i, entry){
-		  $color_selection.append("<option>" + entry.name + "</option>");
-	  });
-	});
-	
-	var selector = "#amount" + s;
-	var $amount_selection = $(selector);
-	  for (i = 0; i < 11; i++) {
-		$amount_selection.append("<option>" + i + "</option>");	
-	}
+/*
+$.getJSON('js/data.json', function(data){
+console.log(data);
+$("#demo").html("JUUUUHU");
+var $secondChoice = $("#second-choice");
+  $(data).each(function(i, hero){
+	  $secondChoice.append("<option>" + hero.Name + "</option>");
+  });
+})
+*/
+
+for (s = 0; s < 5; s++) {
+  	
+
+$.getJSON('js/yarns.json', function(data){
+console.log(data);
+var $yarn_selection = $("#yarn" + s);
+  $yarn_selection.append('<option selected value="base">Bitte auswählen</option>')
+  $(data).each(function(i, entry){
+	  $yarn_selection.append("<option>" + entry.name + "</option>");
+  });
+});
+
+
+$.getJSON('js/colors.json', function(data){
+console.log(data);
+var $color_selection = $("#color" + s);
+  $color_selection.append('<option selected value="base">Bitte auswählen</option>')
+  $(data).each(function(i, entry){
+	  $color_selection.append("<option>" + entry.name + "</option>");
+  });
+});
+
+var $amount_selection = $("#amount" + s);
+  for (i = 0; i < 11; i++) {
+  	$amount_selection.append("<option>" + i + "</option>");	
+}
 
 }
