275e15e963cccc84d468bb1ede9a7f407b5d7df9
Sebastian Loder <36483752+seblod90@users.noreply.github.com>
2020-08-07 19:39:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 
-/*
-$.getJSON('js/data.json', function(data){
-console.log(data);
-$("#demo").html("JUUUUHU");
-var $secondChoice = $("#second-choice");
-  $(data).each(function(i, hero){
-	  $secondChoice.append("<option>" + hero.Name + "</option>");
-  });
-})
-*/
-
-
+// Not possible to loop over all getJSON because asynchronus appearance
 var rows = 5;
 
 $.getJSON('js/yarns.json', function(data){
